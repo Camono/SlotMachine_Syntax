a3bf1efc30300a54cc5f60805846a25f1aec652a
Filip Zivanovic <filip.zivanovic@stud.fh-campuswien.ac.at>
2024-01-08 16:19:22 +0100

    Method to calculate winnings

--- src/main/java/at/ac/fhcampuswien/slotmachine_syntax/Controller/GameManager.java
+++ src/main/java/at/ac/fhcampuswien/slotmachine_syntax/Controller/GameManager.java
@@ -1,6 +1,8 @@
 package at.ac.fhcampuswien.slotmachine_syntax.Controller;
 
+import at.ac.fhcampuswien.slotmachine_syntax.Model.GameResult;
 import at.ac.fhcampuswien.slotmachine_syntax.Model.Symbol;
+import at.ac.fhcampuswien.slotmachine_syntax.Model.SymbolType;
 
 import java.util.*;
 
@@ -128,4 +130,48 @@ public class GameManager {
         // Shouldn't reach here, but return the last element just in case
         return elements.get(elements.size() - 1);
     }
+    public GameResult calculateWinnings(List<Symbol> spinResult) {
+        //das erste Symbol ist ausschlaggebend für das Endergebnis
+        Symbol firstSymbol = spinResult.get(0);
+
+        //kein gewinn
+        if (!spinResult.get(1).equals(firstSymbol) && !spinResult.get(1).getSymbolType().equals(SymbolType.WILD)) {
+            balance = balance - betRange.get(currentBetIndex);
+            return new GameResult(0.0, balance, spinResult);
+        }
+
+        int counter = 0;
+
+        //zählt wie oft ein Symbol von Links nach Rechts vorkommt = Gewinnbedingung
+        for (int i = 1; i < spinResult.size(); i++) {
+            if (spinResult.get(i).equals(firstSymbol) || spinResult.get(i).isWild()) {
+                counter++;
+            }
+        }
+
+        //wenn der counter 2 oder größer ist, gibt es mindestens 3 gleiche symbole von links nach rechts
+        if (counter > 1) {
+            double multiplicator = 0.0;
+
+            switch (counter) {
+                case 2:
+                    multiplicator = firstSymbol.getMultiplierX3();
+                    break;
+                case 3:
+                    multiplicator = firstSymbol.getMultiplierX4();
+                    break;
+                case 4:
+                    multiplicator = firstSymbol.getMultiplierX5();
+                    break;
+            }
+            balance = balance + (betRange.get(currentBetIndex) * multiplicator);
+            return new GameResult(multiplicator * betRange.get(currentBetIndex), balance, spinResult);
+        } else {
+            // wenn der counter 1 oder niedriger ist, ist das Symbol maximal 2 mal erschienen von Links nach rechts
+            //somit gibt es keinen gewinn
+            balance = balance - betRange.get(currentBetIndex);
+            return new GameResult(0.0, balance, spinResult);
+        }
+    }
+
 }
