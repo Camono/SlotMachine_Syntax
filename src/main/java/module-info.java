a052603075574db2e52168f596b39b0e2475516c
Filip Zivanovic <filip.zivanovic@stud.fh-campuswien.ac.at>
2024-01-10 15:16:28 +0100

    Added README.md and cleaned up main branch

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module at.ac.fhcampuswien.slotmachine_syntax {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens at.ac.fhcampuswien.slotmachine_syntax to javafx.fxml;
-    exports at.ac.fhcampuswien.slotmachine_syntax;
-}
